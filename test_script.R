c88282bbe8a6b724f7db4f7f112395f78336e08c
eamon305 <eamon.nils@gmail.com>
2024-10-11 09:15:26 -0300

    Initial commit

---
+++ test_script.R
@@ -0,0 +1,9 @@
+#testing testing
+test = c(1,2,3,4,5)
+
+plot(test)
+
+getwd()
+setwd("/Users/eamon/Library/Mobile Documents/com~apple~CloudDocs/Documents/Important/Academia/GLOFOR/AgroParisTech/Kourou/Computer Tools/GIT/")
+getwd()
+  
