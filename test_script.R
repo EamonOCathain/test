164ccde37ed5ca50696f4a8e9d8f240da923a718
eamon305 <eamon.nils@gmail.com>
2024-10-11 09:22:31 -0300

    test

--- test_script.R
+++ test_script.R
@@ -6,4 +6,4 @@ plot(test)
 getwd()
 setwd("/Users/eamon/Library/Mobile Documents/com~apple~CloudDocs/Documents/Important/Academia/GLOFOR/AgroParisTech/Kourou/Computer Tools/GIT/")
 getwd()
-  
+setwd("/Users/eamon/Library/Mobile Documents/com~apple~CloudDocs/Documents/Important/Academia/GLOFOR/AgroParisTech/Kourou/Linear Modelling/R Projects/test")
